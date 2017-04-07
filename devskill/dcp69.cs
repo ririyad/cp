0eae45bdcc993234a718a981de4f7cb586ed1129
Rashedul Islam <ririyad.291@gmail.com>
2017-04-07 08:32:13 +0600

    solved dcp69 unsolved dcp28

---
+++ devskill/dcp69.cs
@@ -0,0 +1,26 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int testCase = Convert.ToInt32(Console.ReadLine());
+        for(int i = 0; i < testCase; i++)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            int realSum = 0;
+            int kittenSum = 0;
+
+            int[] arr  = new int[n];
+            string[] token = Console.ReadLine().Split(' ');
+            for(int j = 0; j < n; j++)
+            {
+                 arr[j] = Convert.ToInt32(token[j]);
+                 realSum += arr[j];
+                 if(arr[j] >= j)
+                    kittenSum += + arr[j] - j;
+            }
+            Console.WriteLine(realSum - kittenSum);
+        }
+    }
+}
