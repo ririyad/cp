787a5c4fdbf0603d63921bfedd996a4ac809dd04
ririyad <ririyad.291@gmail.com>
2018-03-06 22:41:46 +0600

    soved in code monk

---
+++ hackerearth/CodeMonk/CodeMonk/BirthdayParty/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace BirthdayParty
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int testCase = Convert.ToInt32(Console.ReadLine());
+            for(int i = 0; i < testCase; i++)
+            {
+                string[] tokens = Console.ReadLine().Split();
+                int friends = int.Parse(tokens[0]);
+                int choco = int.Parse(tokens[1]);
+
+                if (choco % friends == 0)
+                    Console.WriteLine("Yes");
+                else
+                    Console.WriteLine("No");
+            }
+        }
+    }
+}
