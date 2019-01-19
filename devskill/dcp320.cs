8c9274cad2cecc4e1f4753a165a58a0c5976a5ca
Rashedul Islam Riyad <ririyad.291@gmail.com>
2019-01-19 22:16:10 +0600

    added solution from ds

---
+++ devskill/dcp320.cs
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	cout << "1" << endl;
+	return 0;
+}#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	cout << "1" << endl;
+	return 0;
+}
