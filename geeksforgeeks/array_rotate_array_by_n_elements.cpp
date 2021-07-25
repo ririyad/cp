a609a5b96c3ececc4ab680e952f1308c2f6446ca
ririyad <rasheduliriyad@gmail.com>
2021-07-25 08:50:11 +0600

    Updated geeksforgeeks solution

--- geeksforgeeks/array_rotate_array_by_n_elements.cpp
+++ geeksforgeeks/array_rotate_array_by_n_elements.cpp
@@ -1,3 +1,30 @@
 // Problem link: https://practice.geeksforgeeks.org/problems/rotate-array-by-n-elements/0#
 
-// To be added soon 
+#include <iostream>
+
+using namespace std;
+
+int main() {
+   ios_base::sync_with_stdio(false);
+   cin.tie(nullptr);
+
+   int t;
+   cin >> t;
+   while(t--) {
+       int n, d;
+       cin >> n >> d;
+       int arr[n];
+       for(int i = 0; i < n; i++) {
+           cin >> arr[i];
+       }
+       int solb[n];
+       for(int i = 0; i < n; i++) {
+           solb[(i+n-d)%n] = arr[i];
+       }
+
+       for(int i = 0; i < n; i++) {
+           cout << solb[i] << " ";
+       }
+       cout << "\n";
+   }
+}
