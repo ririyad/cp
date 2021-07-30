a6f6cecdd6ef5f81cb3dba9941c7d4b220fe3454
ririyad <rasheduliriyad@gmail.com>
2021-07-30 09:57:47 +0600

    Updated new cf problem

--- codeforces/contest/735_div2/A.cpp
+++ codeforces/contest/735_div2/A.cpp
@@ -1,10 +1,27 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
+    int t;
+    cin >> t;
+    while(t--) {
+        int n;
+        cin >> n;
+        vector<int>v(n);
+        for(int i = 0; i< n; i++) {
+            cin >> v[i];
+        }
+        long long ans = 0;
+        for(int i = 1; i < n; i++) {
+            ans = max(ans, 1ll * v[i] * v[i-1]);
+        }
+        cout << ans << "\n";
 
+    }
     
 }
