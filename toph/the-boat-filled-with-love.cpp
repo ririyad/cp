d8edde15bbf555d66b6917347451d0651501719b
ririyad <rasheduliriyad@gmail.com>
2021-08-14 23:39:06 +0600

    updated toph solution

---
+++ toph/the-boat-filled-with-love.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n;
+    while(cin >> n) {
+        cout << "We Love You. We " << n << " Are Here For You." << "\n";
+    }
+}
