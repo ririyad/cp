e8d005723e8f8191acbf613f5d35fc589b553efb
ririyad <rasheduliriyad@gmail.com>
2021-08-16 18:41:36 +0600

    Updated toph solution

---
+++ toph/bignumsum.cpp
@@ -0,0 +1,14 @@
+// Problem link: https://toph.co/p/the-haunted-house
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    long long unsigned a, b;
+    cin >> a >> b;
+
+    cout << (a+b) << "\n";
+}
